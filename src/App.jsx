e1a5a84f9b101d8e345631cdbd4bb3f3beac716c
Sahil Singh Virdhi <sahilsv003@gmail.com>
2023-12-30 23:06:10 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,24 @@ function App() {
       }, 1000);
     }
 
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+    };
   }, [isRunning]);
 
-  const formatTime = (time) => {
-    const minutes = Math.floor(time / 60)
-      .toString()
-      .padStart(2, "0");
-    const seconds = (time % 60).toString().padStart(2, "0");
-    return `${minutes}:${seconds}`;
+  const startStopwatch = () => {
+    setIsRunning(!isRunning);
   };
 
-  const handleStartStop = () => {
-    setIsRunning((prevIsRunning) => !prevIsRunning);
-  };
-
-  const handleReset = () => {
-    setIsRunning(false);
+  const resetStopwatch = () => {
     setTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
   return (
@@ -39,10 +39,8 @@ function App() {
       <header>Stopwatch</header>
       <div className="time">Time: {formatTime(time)}</div>
       <div className="buttons">
-        <button onClick={handleStartStop}>
-          {isRunning ? "Stop" : "Start"}
-        </button>
-        <button onClick={handleReset}>Reset</button>
+        <button onClick={startStopwatch}>{isRunning ? "Stop" : "Start"}</button>
+        <button onClick={resetStopwatch}>Reset</button>
       </div>
     </div>
   );
